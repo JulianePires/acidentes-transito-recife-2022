573fa2b2ad20602b0ca2422c2e8cd281aa935d3d
Juliane Pires <mpiresjuliane@outlook.com>
2024-02-03 11:37:02 -0300

    Ajustes finais no dashboard

--- app.py
+++ app.py
@@ -1,11 +1,61 @@
+import pandas as pd
 import streamlit as st
+import plotly.express as px
+from streamlit_extras.metric_cards import style_metric_cards 
 
-DADOS_URL = "./acidentes2022.csv"
+# Lendo a base de dados
+df = pd.read_csv("acidentes2022.csv", on_bad_lines="skip", sep=";")
 
-st.title("Acidentes de trânsito com e sem vitimas na cidade de Recife - 2022")
-st.markdown(
-    "Esta aplicação é um dashboard Streamlit que pode ser utilizado "
-    "para analise das estatísticas dos acidentes de trânsito na cidade de Recife 🏙💥🚗"
-)
+df["vitimasfatais"] = df["vitimasfatais"].str.replace(",", ".").astype(float)
+df["vitimas"] = df["vitimas"].str.replace(",", ".").astype(float)
 
-## Indices de vítimas por bairro
+clima = df["tempo_clima"].value_counts().sort_values(ascending=False)
+
+bairro = df["bairro"].value_counts().head(10).sort_values(ascending=True)
+
+# alterando coluna de data para datetime
+df["data"] = pd.to_datetime(df["data"])
+
+# Criando coluna de mês
+df["Mês_Acidente"] = df["data"].dt.month
+
+total_mes = df["Mês_Acidente"].value_counts().reset_index()
+total_mes = total_mes.sort_values(by="Mês_Acidente")
+
+def main():
+
+    st.header("Acidentes de Trânsito em Recife - 2022")
+    #st.markdown("<h1 style='text-align: center;'>Acidentes de trânsito em Recife</h1>", #unsafe_allow_html=True)
+
+    total_acidentes = df.shape[0]
+    total_com_vitimas = "{:.0f}".format(df["vitimas"].sum())
+    total_vitimas_fatais = "{:.0f}".format(df["vitimasfatais"].sum())
+
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Total Acidentes 🚦", total_acidentes)
+    style_metric_cards(border_left_color="#7CCCE5", background_color="inherit !important")
+
+    col2.metric("Total com vítimas 🚨", total_com_vitimas)
+    col3.metric("Total vítimas fatais 🪦", total_vitimas_fatais)
+
+
+    fig = px.bar(clima, text=clima.values, color_discrete_sequence=["#7CCCE5"])
+    fig.update_layout(title="Total de acidentes por Clima", title_x=0.1,showlegend=False)
+    st.plotly_chart(fig)
+
+    fig1 = px.bar(bairro, text=bairro.values,
+             color_discrete_sequence=["#7CCCE5"], orientation="h")
+    fig1.update_layout(title="Top 10 acidentes por Bairro", title_x=0.1,showlegend=False)
+    st.plotly_chart(fig1)
+
+    
+    fig2 = px.line(total_mes, x="Mês_Acidente", y="count",
+              color_discrete_sequence=["#7CCCE5"], markers=True,
+              text="count", labels={"Mês":"Mês Acidente", "Total":"Total Acidentes"})
+    fig2.update_layout(title='Total de acidentes por mês', title_x=0.5)
+    fig2.update_traces(textposition='top center')
+
+    st.plotly_chart(fig2)
+
+if __name__ == "__main__":
+    main()
